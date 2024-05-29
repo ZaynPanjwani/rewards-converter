48789d103b0968d7c013335675cb02ed2bf2fb65
zaynpanjwani <zaynpanjwani@gmail.com>
2024-05-29 14:38:10 +0100

    done testing

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,31 +1,28 @@
-import java.util.Scanner;
+public class RewardValue {
+    private final double cashValue;
+    public static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
-public class RewardValue{
-    private double cashValue;
-    private int milesValue;
-    
-
-    public RewardValue(double cashValue)
-    {
-        double rate = 0.0035;
-        this.milesValue = (int)(cashValue/rate);
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
     }
 
-    public RewardValue(int milesValue)
-    {
-        double rate = 0.0035;
-        this.cashValue = ((double)milesValue) * rate;
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
     }
 
-    public double getCashValue()
-    {
-        return this.cashValue;
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
     }
 
-    public int getMilesValue()
-    {
-        return this.milesValue;
+    private static double convertToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 
+    public double getCashValue() {
+        return cashValue;
+    }
 
+    public int getMilesValue() {
+        return convertToMiles(this.cashValue);
+    }
 }
